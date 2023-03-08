a60ff79b92d48ea260bfe2b0c63ae7e8cb571c81
fersie7e <fersie7e@gmail.com>
2023-03-08 17:43:52 +0100

    Devolver temperatura para cualquier estacion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import flask
-
+import pandas as pd
 
 app = flask.Flask(__name__)
 
@@ -8,8 +8,10 @@ def home():
     return flask.render_template("home.html")
 
 @app.route("/api/v1/<station>/<date>")
-def about(station, date):
-    temperature = 23
+def api(station, date):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=['    DATE'])
+    temperature = df.loc[df['    DATE'] == date]['   TG'].squeeze() / 10
     return {"station": station,
             "date": date,
             "temperatura": temperature}
